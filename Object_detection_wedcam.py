83caf70e597dd9cde89ecb5e8267d94adae0dae4
soixam0102 <sanamata085@gmail.com>
2021-06-03 10:09:52 +0700

    completed commit

---
+++ Object_detection_wedcam.py
@@ -0,0 +1,44 @@
+# this is program for object detection
+# "coco.names" contains class of object
+# "frozen_inference_graph.ph" contain weigh
+# "ssd_mobilenet_v3_large_coco_2021_01_14.pbtxt" contain the architecture of that network
+# ssd_mobinet run without GPU and almost realtime, YOLO need GPU and YOLO tiny is not good enough
+# detecting object by using wed-cam
+
+import cv2
+
+
+cap = cv2.VideoCapture(1)
+cap.set(3,1280)
+cap.set(4,720)
+cap.set(10,150)
+
+
+classNames = []
+classFile = 'coco.names'
+with open(classFile, 'rt') as f:
+    classNames = f.read().rstrip('\n').split('\n')  # strip in new line and split in new line
+
+# load the model of net
+configPath = 'ssd_mobilenet_v3_large_coco_2020_01_14.pbtxt'
+weightsPath = 'frozen_inference_graph.pb'
+net = cv2.dnn_DetectionModel(weightsPath, configPath)
+net.setInputSize(320, 320)
+net.setInputScale(1.0 / 127.5)
+net.setInputMean((127.5, 127.5, 127.5))
+net.setInputSwapRB(True)
+
+
+while True:
+    success, img = cap.read()
+    classIds, confs, bbox = net.detect(img, confThreshold=0.5)
+    print(classIds, bbox)
+
+    if len(classIds) != 0:
+
+        for classId, confidence, box in zip(classIds.flatten(), confs.flatten(), bbox):
+            cv2.rectangle(img, box, color=(0, 0, 222), thickness=2)
+            cv2.putText(img, classNames[classId - 1].upper(), (box[0], box[1]), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 222, 0))
+
+    cv2.imshow('Output', img)
+    cv2.waitKey(1)
